14aeb13d1f0793c9f12375d25d8085038b1c51ce
desyncfy <chickenmanfy@proton.me>
2025-01-04 19:56:41 -0800

    Do setup for mcpixelate

--- screentominecraft.py
+++ screentominecraft.py
@@ -1,1 +1,6 @@
 
+# Thank you mcpixelate, you're exactly what I need and you have 1 (now 2) star and no updates for 5 years.
+from mcpixelate.mcpixelate.mcpixelate import PixelImage
+
+image = PixelImage("Image.png", 64, 0, 0)
+image.save_image("Image_Pixelated.png")
